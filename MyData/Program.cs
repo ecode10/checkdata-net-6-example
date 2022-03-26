4b14dd918f159d24b6ff12099026dba1b4ae2b1d
Mauricio Junior <mauricio.junior@gmail.com>
2022-03-26 15:37:29 -0500

    add project

---
+++ MyData/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Mail;
+using CheckData;
+
+IMain main = new Main();
+var content = "MauricioJunior82*&";
+var result = main.IsURL("2098slskjslk");
+
+Console.WriteLine("Is URL? " + result);
+
+var letters = main.ReturnLetters(content);
+
+Console.WriteLine("Letters: " + letters);
+
+var numbers = main.ReturnNumber(content);
+
+Console.WriteLine($"Numbers: {numbers}");
+
+var lowercase = main.ReturnLowerCase(content);
+
+Console.WriteLine($"Lower: {lowercase}");
